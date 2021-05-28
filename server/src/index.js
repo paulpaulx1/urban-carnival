836ccdff1675e487d7452df2458232cb2d031927
paul <paulmneenan@gmail.com>
2021-05-28 12:47:12 -0400

    gitigno

--- server/src/index.js
+++ server/src/index.js
@@ -1,10 +1,15 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
 
 app.get('/', (req, res, next) => {
-    res.send('hellworld')
+  try {
+    res.send('hellworld');
+  } catch (err) {
+    next();
+  }
 });
 
+
 app.listen(3005, () => {
-    console.log('App listening on port 3005!');
+  console.log('App listening on port 3005!');
 });
